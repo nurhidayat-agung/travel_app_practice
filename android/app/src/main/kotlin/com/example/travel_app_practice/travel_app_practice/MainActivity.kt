ef4b600067e70e770b6044cb3d61de2288b0dfeb
agung.hidayat <agung.hidayat@kalbenutritionals.com>
2022-05-08 06:35:49 +0700

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/travel_app_practice/travel_app_practice/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.travel_app_practice.travel_app_practice
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/pages/welcome_page.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class WelcomePage extends StatefulWidget {
+  const WelcomePage({Key? key}) : super(key: key);
+
+  @override
+  State<WelcomePage> createState() => _WelcomePageState();
+}
+
+class _WelcomePageState extends State<WelcomePage> {
+
+  List images = [
+    "welcome-one.png",
+    "welcome-two.png",
+    "welcome-three.png",
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: PageView.builder(
+          scrollDirection: Axis.vertical,
+          itemCount: images.length,
+          itemBuilder: (_, index) {
+            return Container(
+              width: double.maxFinite,
+              height: double.maxFinite,
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage(
+                    "img/" + images[index]
+                  )
+                )
+              ),
+            );
+          }),
+    );
+  }
+}
